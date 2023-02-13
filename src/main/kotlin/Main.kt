71792da42d1870e648a2f5419e3de61f8d2189fa
Ruben Hernandez <123031291+Ruben935@users.noreply.github.com>
2023-02-12 19:47:59 -0600

    02.2 Variable Declaration and Initialization

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>)
+{
+    var myAge = 21
+    var averageAge = 21
+    var testNumber = 100
+    var evenOdd = testNumber
+    testNumber - myAge
+    var answer = 0
+    answer + 1
+    answer + 10
+    answer * 10
+    averageAge = (30 - myAge) / 2
+    println("My age is: " + myAge)
+    println("The average age is: " + averageAge)
+    println("The test number is: " + testNumber)
+    println("even odd is: " + evenOdd)
+    println("answer is: " + answer)
+
+
+}
