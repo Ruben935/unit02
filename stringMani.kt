49872e94d15ab31523b40d7bc8d218ca5e561cbd
Ruben Hernandez <123031291+Ruben935@users.noreply.github.com>
2023-02-12 22:55:03 -0600

    Create stringMani.kt

---
+++ stringMani.kt
@@ -0,0 +1,13 @@
+//Ruben Hernandez
+//02.8 String Manipulator
+//2/12/2023
+
+fun main(args: Array<String>)
+{
+    println("Enter the name of your favorite city: ")
+    var city = readLine()
+    println(city.length)
+    println(city.toUpperCase())
+    println(city.toLowerCase())
+    println(city[0])
+}
