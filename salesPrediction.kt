cb073d7ee455f78941316bb2ffa0783b38eeab1f
Ruben Hernandez <123031291+Ruben935@users.noreply.github.com>
2023-02-12 21:36:59 -0600

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>)
+{
+    var yearSales: Double = 4600000.0
+    var answer: Double = yearSales * 0.62
+    print("The East Coast will generate: " + answer + " from the total sales.")
+}
