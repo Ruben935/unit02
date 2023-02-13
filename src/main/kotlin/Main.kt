ddfedf4184e775552b23c2b9a30ec824267a6be7
Ruben Hernandez <123031291+Ruben935@users.noreply.github.com>
2023-02-12 21:04:52 -0600

    02.4 Name, Age, and Annual Income

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>)
+{
+    var name: String = "Ruben"
+    var age: Int = 21
+    var annualPay: Double = 80000.0
+    println("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.")
+}
